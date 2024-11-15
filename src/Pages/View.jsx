e30058f2fa4ae7e5636e9dd05ae3fe2979490496
Faheema_Fasil <faheemafasil12@gmail.com>
2024-11-15 12:00:05 +0530

    wishlist created

--- src/Pages/View.jsx
+++ src/Pages/View.jsx
@@ -2,9 +2,18 @@ import React from 'react'
 
 function View() {
   return (
-    <div>
-      
-    </div>
+    <>
+      <div className="container mt-5 row">
+        <div className="col-lg-4">
+          <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRxeH5g4bVSM2f-gSIu28vo1QGQceIr0Qbf6g&s" alt="" />
+        </div>
+        <div className="col-lg-2"></div>
+        <div className="col-lg-6">
+          <p>Pid:1</p>
+          <h1></h1>
+        </div>
+      </div>
+    </>
   )
 }
 
--- src/Pages/Wishlist.jsx
+++ src/Pages/Wishlist.jsx
@@ -1,10 +1,27 @@
 import React from 'react'
+import { Button, Card } from 'react-bootstrap'
 
 function Wishlist() {
   return (
-    <div>
-      
-    </div>
+    <>
+      <div style={{ marginTop: "50px" }} className='container-fluid'>
+        <Card style={{ width: '18rem' }}>
+          <Card.Img variant="top" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRxeH5g4bVSM2f-gSIu28vo1QGQceIr0Qbf6g&s" />
+          <Card.Body>
+            <Card.Title>Card Title</Card.Title>
+            <Card.Text>
+              Some quick example text to build on the card title and make up the
+              bulk of the card's content.
+            </Card.Text>
+            <div className="d-flex justify-content-between">
+              <Button variant="primary"> <i class="fa-solid fa-trash" style={{ "color": "#ffffff" }}></i></Button>
+              <Button variant="primary"><i className="fa-sharp-duotone fa-solid fa-cart-plus" style={{ "--fa-primary-color": "#5b5848", "--fa-secondary-color": "#5b5848" }}></i></Button>
+            </div>
+
+          </Card.Body>
+        </Card>
+      </div>
+    </>
   )
 }
 
