90acbf49ae3b87731533e47bac3d6be80a15849e
Faheema_Fasil <faheemafasil12@gmail.com>
2024-11-15 11:09:31 +0530

    card created

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,10 +1,47 @@
 import React from 'react'
+import { Badge, Button,Form } from 'react-bootstrap';
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import {  Link } from 'react-router-dom';
 
 function Header() {
   return (
-    <div>
-      
-    </div>
+    <>
+      <Navbar expand="lg" className="bg-body-tertiary">
+        <Container>
+          <Navbar.Brand >
+            <Link to="/" style={{textDecoration:"none",fontSize:"30px"}}><i class="fa-solid fa-shop fa-bounce"></i> E-Cart</Link>
+          </Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            {/* search  */}
+          <Form.Control
+              type="text"
+              placeholder="Search"
+              className='w-25 ms-5'
+            />
+            <Nav className="ms-auto">
+              <Nav.Link >
+                <Link to="/wishlist">
+                  <Button variant="primary">
+                  <i class="fa-solid fa-heart" style={{"color": "#ffffff"}}></i> Wishlist <Badge bg="secondary">0</Badge>
+                  </Button>
+                </Link>
+              </Nav.Link>
+              <Nav.Link >
+                <Link to="/cart">
+                  <Button variant="primary">
+                  <i className="fa-sharp-duotone fa-solid fa-cart-shopping" style={{"--fa-primary-color": "#5b5848", "--fa-secondary-color": "#5b5848"}}></i> Cart <Badge bg="secondary">0</Badge>
+                  </Button>
+                </Link>
+              </Nav.Link>
+             
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </>
   )
 }
 
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,9 +1,24 @@
 import React from 'react'
+import { Button, Card } from 'react-bootstrap'
 
 function Home() {
   return (
-    <div>
-      
+    <div style={{marginTop:"50px"}} className='container-fluid'>
+      <Card style={{ width: '18rem' }}>
+      <Card.Img variant="top" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRxeH5g4bVSM2f-gSIu28vo1QGQceIr0Qbf6g&s" />
+      <Card.Body>
+        <Card.Title>Card Title</Card.Title>
+        <Card.Text>
+          Some quick example text to build on the card title and make up the
+          bulk of the card's content.
+        </Card.Text>
+        <div className="d-flex justify-content-between">
+        <Button variant="primary"> <i class="fa-solid fa-heart" style={{"color": "#ffffff"}}></i></Button>
+        <Button variant="primary"><i className="fa-sharp-duotone fa-solid fa-cart-shopping" style={{"--fa-primary-color": "#5b5848", "--fa-secondary-color": "#5b5848"}}></i></Button>
+        </div>
+       
+      </Card.Body>
+    </Card>
     </div>
   )
 }
