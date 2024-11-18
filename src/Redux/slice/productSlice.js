e802f87db28aad4316e3c5a682478e106d85e65b
Faheema_Fasil <faheemafasil12@gmail.com>
2024-11-18 12:45:26 +0530

    extraReducers added

--- src/Redux/slice/productSlice.js
+++ src/Redux/slice/productSlice.js
@@ -1,6 +1,8 @@
 import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 import axios from "axios";
-const fetchProducts=createAsyncThunk("products/fetchProducts",async()=>{
+
+
+export const fetchProducts=createAsyncThunk("products/fetchProducts",async()=>{
     const result = await axios.get("https://dummyjson.com/products")
     return result.data.products
 })
@@ -21,6 +23,16 @@ const productSlice=createSlice({
             state.allproducts=action.payload
             state.loading=false
             state.error=""
+        })
+        builder.addCase(fetchProducts.pending,(state,action)=>{
+            state.allproducts=[]
+            state.loading=true
+            state.error=""
+        })
+        builder.addCase(fetchProducts.rejected,(state,action)=>{
+            state.allproducts=[]
+            state.loading=false
+            state.error="API call failed..."
         })
     }
 })
