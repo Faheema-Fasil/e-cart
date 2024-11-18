ea6df0247451232ce4a81877fe72faffb15aeb0a
Faheema_Fasil <faheemafasil12@gmail.com>
2024-11-18 12:29:15 +0530

    created async thunk

--- src/Redux/slice/productSlice.js
+++ src/Redux/slice/productSlice.js
@@ -1,13 +1,27 @@
-import { createSlice } from "@reduxjs/toolkit";
-
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import axios from "axios";
+const fetchProducts=createAsyncThunk("products/fetchProducts",async()=>{
+    const result = await axios.get("https://dummyjson.com/products")
+    return result.data.products
+})
 
 
 const productSlice=createSlice({
     name:"products",
     initialState:{
-        allproducts:[]
+        allproducts:[],
+        loading:false,
+        error:""
     },
     reducers:{
 
+    },
+    extraReducers:(builder)=>{
+        builder.addCase(fetchProducts.fulfilled,(state,action)=>{
+            state.allproducts=action.payload
+            state.loading=false
+            state.error=""
+        })
     }
 })
+export default productSlice.reducer
